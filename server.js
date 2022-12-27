d0a8ec372bf0fcd45fe7b4ae7c7e06fa0522ac13
Anass knene <anas.knene@hotmail.com>
2022-12-27 03:56:56 +0100

    add static code to serve build file

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ const cors = require("cors");
 //Don't forget run express.json middleware to get data as json form.
 app.use(express.json());
 //To connect both front-end and back-end, you should run a middle ware called 'cors', to prevent a cors error.
-app.use(cors())
+app.use(cors());
 //it allows us to use  .env file.
 dotenv.config();
 
 const port = process.env.PORT || 4000;
+
+app.use(express.static(__dirname + "/backend/build"));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/backend/build/index.html");
+});
 
 app.use("/planets", PlanetsRoute);
 app.use("/add", addRoute);
